ecd84e4195ad61f833ba054a3de46e88b6442bd6
Sara <Sara.Ginsburg.ny@gmail.com>
2023-11-28 13:02:01 -0500

    create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Home from './pages/Home';
 import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
-import About from './pages/about';
+import About from './pages/About';
+import Header from './components/Header';
 //checking git access
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
