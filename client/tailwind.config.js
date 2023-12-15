2b8644552277d4d7d57e8ede4ce171cd33b05323
Sara <Sara.Ginsburg.ny@gmail.com>
2023-12-14 21:31:44 -0500

    image upload functionality

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Add or modify colors here
+        customGreen: '#809e88', // Example RGB value in hex
+      },
+    },
   },
   plugins: [],
 };
