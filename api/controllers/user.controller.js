b582ff8d5717ecf6ebb8260e0a0bcb59b599aaf7
Sara <Sara.Ginsburg.ny@gmail.com>
2023-12-17 17:02:55 -0500

    create updateUser api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,43 @@
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res) =>
   res.json({
     message: 'testing, testing...',
   });
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    //Update the user in the database
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true } //Return the updated document
+    );
+
+    //Check if the user was not found
+    if (!updatedUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
