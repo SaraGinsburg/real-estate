4fdd4a44f86ca9dc4d427120508ce2b186f25225
Sara <Sara.Ginsburg.ny@gmail.com>
2024-02-05 20:29:07 -0500

    complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
-
 export default function About() {
-  return <div>about</div>;
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-600'>
+        About Gani Estates
+      </h1>
+      <p className='text-slate-500 mb-4 '>
+        Gani Estates, a prominent real estate agency, excels in assisting
+        clients with buying, selling, and renting properties in sought-after
+        neighborhoods. Our team of seasoned agents is committed to delivering
+        exceptional service, ensuring a seamless experience in the buying and
+        selling process.
+      </p>
+
+      <p className='text-slate-500 mb-4 '>
+        We are dedicated to assisting clients in reaching their real estate
+        goals through expert advice, personalized service, and a profound
+        knowledge of the local market. Whether you are buying, selling, or
+        renting, we are with you at every step.
+      </p>
+      <p className='text-slate-500 mb-4 '>
+        With extensive experience and expertise in the real estate industry, our
+        agent team is devoted to delivering top-notch service to our clients. We
+        firmly believe that the process of buying or selling a property should
+        be both thrilling and fulfilling, and we are steadfast in turning that
+        belief into a reality for each client we serve.
+      </p>
+    </div>
+  );
 }
